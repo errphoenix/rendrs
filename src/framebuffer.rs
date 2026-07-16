a740d3c6898da91314d7d09480f3586cdf404266
errphoenix <herrphoenix20@gmail.com>
2026-07-17 01:41:26 +0200

    framebuffer: finalize Framebuffer abstractions

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -26,6 +26,11 @@ impl From<FramebufferView> for FramebufferId {
         value.id
     }
 }
+impl From<&FramebufferView> for FramebufferId {
+    fn from(value: &FramebufferView) -> Self {
+        value.id
+    }
+}
 impl From<&Framebuffer> for FramebufferId {
     fn from(value: &Framebuffer) -> Self {
         value.id
@@ -34,6 +39,134 @@ impl From<&Framebuffer> for FramebufferId {
 
 type ColorAttachments = tinyvec::ArrayVec<[TextureView; MAX_ATTACHMENTS]>;
 type DepthAttachment = Option<TextureView>;
+
+pub trait HasFramebuffer {
+    fn id(&self) -> FramebufferId;
+    fn width(&self) -> u32;
+    fn height(&self) -> u32;
+    fn color_attachments(&self) -> &ColorAttachments;
+    fn depth_attachment(&self) -> &DepthAttachment;
+
+    fn bind(&self) {
+        crate::framebuffer::bind(self.id());
+    }
+
+    fn unbind() {
+        crate::framebuffer::bind_default();
+    }
+
+    fn color_attachments_len(&self) -> usize {
+        self.color_attachments().len()
+    }
+
+    fn color_attachment(&self, index: usize) -> TextureView {
+        self.color_attachments()[index]
+    }
+
+    fn has_depth(&self) -> bool {
+        self.depth_attachment().is_some()
+    }
+
+    /// Sets the default read/write buffers state for this Framebuffer.
+    ///
+    /// * If the Framebuffer has no color attachments:
+    ///     The Framebuffer is interpreted as being for a depth-only rendering
+    ///     pass, so `GL_NONE` is bound to draw and read buffers at target 0.
+    /// * Otherwise:
+    ///     Each attachment is mapped linearly to writing buffers (i.e.,
+    ///     attachment at index `N` is mapped to target `N` and so on), while
+    ///     attachment 0 is bound to the read buffer target.
+    ///
+    /// Note that OpenGL's read buffer only has a single target.
+    fn set_default_buffers_state(&self) {
+        if self.color_attachments().is_empty() {
+            // this is a depth-only pass
+            self.set_write_buffer(None);
+            self.set_read_buffer(None);
+        } else {
+            // map color attachments to draw buffers linearly
+            let draw_buffers = std::array::from_fn::<_, MAX_ATTACHMENTS, _>(|i| Some(i as u32));
+            self.set_write_buffers(&draw_buffers);
+            self.set_read_buffer(Some(0));
+        }
+    }
+
+    /// Sets the read attachment for blit operations.
+    ///
+    /// Passing `None` will set the target to `GL_NONE`.
+    ///
+    /// Note that OpenGL's read buffer only has a single target.
+    fn set_read_buffer(&self, attachment_index: Option<u32>) {
+        janus::debug_assert_gl!();
+
+        #[cfg(debug_assertions)]
+        if let Some(index) = attachment_index {
+            debug_assert!((index as usize) < self.color_attachments_len());
+        }
+
+        let index = attachment_index.map_or(janus::gl::NONE, |i| janus::gl::COLOR_ATTACHMENT0 + i);
+        unsafe {
+            janus::gl::NamedFramebufferReadBuffer(self.id().0, index);
+        }
+    }
+
+    /// Sets the write attachment at shader target 0.
+    ///
+    /// Passing `None` will set the target to `GL_NONE`.
+    fn set_write_buffer(&self, attachment_index: Option<u32>) {
+        janus::debug_assert_gl!();
+
+        #[cfg(debug_assertions)]
+        if let Some(index) = attachment_index {
+            debug_assert!((index as usize) < self.color_attachments_len());
+        }
+
+        let index = attachment_index.map_or(janus::gl::NONE, |i| janus::gl::COLOR_ATTACHMENT0 + i);
+        unsafe {
+            janus::gl::NamedFramebufferDrawBuffer(
+                self.id().0,
+                janus::gl::COLOR_ATTACHMENT0 + index,
+            );
+        }
+    }
+
+    /// Sets the write attachments for an arbitrary number of shader targets.
+    ///
+    /// The index of each element in the given `attachment_indices` array
+    /// equals to the shader target index. The value of each entry is instead
+    /// the index of the attachment of this Framebuffer to be attached to that
+    /// target.
+    ///
+    /// For each `None` element, the target at the index will be set to
+    /// `GL_NONE`.
+    fn set_write_buffers(&self, attachment_indices: &[Option<u32>]) {
+        janus::debug_assert_gl!();
+
+        #[cfg(debug_assertions)]
+        for &i in attachment_indices {
+            if let Some(i) = i {
+                debug_assert!((i as usize) < self.color_attachments_len());
+            }
+        }
+
+        let attachment_indices = attachment_indices
+            .iter()
+            .map(|index| {
+                if let Some(index) = index {
+                    janus::gl::COLOR_ATTACHMENT0 + index
+                } else {
+                    janus::gl::NONE
+                }
+            })
+            .collect::<tinyvec::ArrayVec<[u32; MAX_ATTACHMENTS]>>();
+
+        let ptr = attachment_indices.as_ptr();
+        let len = attachment_indices.len();
+        unsafe {
+            janus::gl::NamedFramebufferDrawBuffers(self.id().0, len as i32, ptr);
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct FramebufferView {
@@ -58,17 +191,26 @@ impl FramebufferView {
             depth_attachment: framebuffer.depth_attachment,
         }
     }
-
-    pub const fn id(&self) -> FramebufferId {
+}
+impl HasFramebuffer for FramebufferView {
+    fn id(&self) -> FramebufferId {
         self.id
     }
 
-    pub const fn width(&self) -> u32 {
+    fn width(&self) -> u32 {
         self.width
     }
 
-    pub const fn height(&self) -> u32 {
+    fn height(&self) -> u32 {
         self.height
+    }
+
+    fn color_attachments(&self) -> &ColorAttachments {
+        &self.color_attachments
+    }
+
+    fn depth_attachment(&self) -> &DepthAttachment {
+        &self.depth_attachment
     }
 }
 
@@ -189,36 +331,29 @@ impl Framebuffer {
         })
     }
 
-    pub fn bind(&self) {
-        crate::framebuffer::bind(self);
+    pub const fn as_view(&self) -> FramebufferView {
+        FramebufferView::from_framebuffer(self)
     }
-
-    pub fn unbind() {
-        crate::framebuffer::bind_default();
-    }
-
-    pub const fn id(&self) -> FramebufferId {
+}
+impl HasFramebuffer for Framebuffer {
+    fn id(&self) -> FramebufferId {
         self.id
     }
 
-    pub const fn width(&self) -> u32 {
+    fn width(&self) -> u32 {
         self.width
     }
 
-    pub const fn height(&self) -> u32 {
+    fn height(&self) -> u32 {
         self.height
     }
 
-    pub fn color_attachment(&self, index: usize) -> TextureView {
-        self.color_attachments[index]
+    fn color_attachments(&self) -> &ColorAttachments {
+        &self.color_attachments
     }
 
-    pub fn depth_attachment(&self) -> Option<TextureView> {
-        self.depth_attachment
-    }
-
-    pub const fn as_view(&self) -> FramebufferView {
-        FramebufferView::from_framebuffer(self)
+    fn depth_attachment(&self) -> &DepthAttachment {
+        &self.depth_attachment
     }
 }
 impl Drop for Framebuffer {
