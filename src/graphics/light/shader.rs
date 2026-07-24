095ec6f5d3e6bcd19e82187bb3b7d8e93c0ddf6d
errphoenix <herrphoenix20@gmail.com>
2026-07-24 18:21:33 +0200

    docs(light): list function parameters of light attenuation ethel shader glsl lib functions

--- src/graphics/light/shader.rs
+++ src/graphics/light/shader.rs
@@ -30,8 +30,20 @@ pub const CONST_LIGHT_FLAG_OMNI: Constant<f32> =
 pub const CONST_LIGHT_FLAG_SPOTLIGHT: Constant<f32> =
     Constant::new("LIGHT_FLAG_SPOTLIGHT", super::KIND_FLAG_SPOTLIGHT);
 
+/// Light attenuation function through an inverse-squared windowed curve.
+///
+/// Creates the `lightAttenuate` function with the following parameters:
+/// * `float sq_light_dist` the squared distance to the light.
+/// * `float light_dist` the distance to the light.
+/// * `float window_max` the length of the window or maximum affected
+///    distance of the light.
+/// * `float eps` a small epsilon to avoid singularities in the curve;
+///    depends on scene scale.
+///
+/// This is exclusive with [`LIB_LIGHT_ATTENUATE_DISTANCE_FALLOFF`] and
+/// shaders will fail to compile if they are both defined.
 pub const LIB_LIGHT_ATTENUATE_ISQ_WINDOWED_CURVE: GlslLib = ethel::shader_glsl_lib! {
-    float lightAttenuateISQWindowedCurve [
+    float lightAttenuate [
         sq_light_dist : float,
         light_dist    : float,
         window_max    : float,
@@ -45,10 +57,20 @@ pub const LIB_LIGHT_ATTENUATE_ISQ_WINDOWED_CURVE: GlslLib = ethel::shader_glsl_l
     "
 };
 
+/// Light attenuation function through a simple distance-falloff curve.
+///
+/// Creates the `lightAttenuate` function with the following parameters:
+/// * `float light_dist` the distance to the light.
+/// * `float max_max` the maximum affected distance of the light.
+///
+/// This is exclusive with [`LIB_LIGHT_ATTENUATE_ISQ_WINDOWED_CURVE`] and
+/// shaders will fail to compile if they are both defined.
+///
+/// It is generally cheaper than [`LIB_LIGHT_ATTENUATE_ISQ_WINDOWED_CURVE`].
 pub const LIB_LIGHT_ATTENUATE_DISTANCE_FALLOFF: GlslLib = ethel::shader_glsl_lib! {
-    float lightAttenuateDistanceFalloff [
+    float lightAttenuate [
         light_dist    : float,
-        max_dist    : float
+        max_dist      : float
     ] => "
         float t = light_dist / max_dist;
         float u = 1.0 - (t * t);
