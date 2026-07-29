09c377aee8e7f9c0424bec4217d4a01cca0d0920
errphoenix <herrphoenix20@gmail.com>
2026-07-29 03:06:17 +0200

    feat(materials): complete material groups initialization system

--- src/graphics/material/mod.rs
+++ src/graphics/material/mod.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
-use ethel::assets::{AssetId, RawTexture};
+use ethel::assets::{AssetId, AssetRegistry, RawTexture, TextureMetadata};
 use image::{EncodableLayout, RgbaImage};
 use janus::{
     StringHash, StringMap,
-    texture::{ImageFormat, ImageType, Tex, Texture, TextureView},
+    texture::{ImageFormat, ImageType, MipLevels, Tex, Texture, TextureView},
 };
 
 use crate::pipeline::SamplerObject;
@@ -282,7 +282,7 @@ macro_rules! material_groups {
 
 #[derive(Clone, Debug)]
 pub struct MaterialGroupDescriptor {
-    pub group_index: u32,
+    pub group_index: u16,
     pub pages: u16,
     pub size: u16,
 
@@ -292,10 +292,13 @@ pub struct MaterialGroupDescriptor {
 
     /// Cached page indices allocated to entries that were already processed,
     /// avoiding duplicate entries in the final material texture array.
-    pub cached_entries: HashMap<MaterialEntryDescriptor, u16>,
+    ///
+    /// The cache also stores the sub- width and height of each entry texture,
+    /// which are only set at the final build step.
+    pub cached_entries: HashMap<MaterialEntryDescriptor, MaterialEntryCache>,
 }
 impl MaterialGroupDescriptor {
-    pub fn new(group_index: u32, pages: u16, size: u16) -> Self {
+    pub fn new(group_index: u16, pages: u16, size: u16) -> Self {
         Self {
             group_index,
             pages,
@@ -312,7 +315,8 @@ impl MaterialGroupDescriptor {
 
     pub fn distribute_pages(&mut self) {
         self.cached_entries.clear();
-        let mut page_i = 0;
+        // page 0 is reserved for default
+        let mut page_i = 1;
         self.desciptors.values().for_each(
             |&MaterialDescriptor {
                  diffuse_emissive,
@@ -321,7 +325,13 @@ impl MaterialGroupDescriptor {
                 match diffuse_emissive {
                     Some(de) => {
                         let entry = MaterialEntryDescriptor::DiffuseEmissive(de);
-                        self.cached_entries.insert(entry, page_i);
+                        self.cached_entries.insert(
+                            entry,
+                            MaterialEntryCache {
+                                assigned_page_index: page_i,
+                                ..Default::default()
+                            },
+                        );
                         page_i += 1;
                     }
                     _ => {}
@@ -329,7 +339,13 @@ impl MaterialGroupDescriptor {
                 match rsod {
                     Some(rsod) => {
                         let entry = MaterialEntryDescriptor::Rsod(rsod);
-                        self.cached_entries.insert(entry, page_i);
+                        self.cached_entries.insert(
+                            entry,
+                            MaterialEntryCache {
+                                assigned_page_index: page_i,
+                                ..Default::default()
+                            },
+                        );
                         page_i += 1;
                     }
                     _ => {}
@@ -349,11 +365,169 @@ impl MaterialGroupDescriptor {
             self.pages - page_i
         );
     }
+
+    pub fn process_locations(&mut self) {
+        self.materials.clear();
+
+        let default_entry_loc = MaterialEntryLocation {
+            group_index: self.group_index,
+            page: 0,
+        };
+
+        self.desciptors.iter().for_each(|(&id, descriptor)| {
+            let mut material_location = MaterialLocation {
+                diffuse_and_emissive: default_entry_loc,
+                rsod_entry: default_entry_loc,
+                // initialized later
+                width: 0f32,
+                height: 0f32,
+            };
+
+            if let Some(diffuse_emissive) = descriptor.diffuse_emissive {
+                let entry = MaterialEntryDescriptor::DiffuseEmissive(diffuse_emissive);
+                if let Some(cache) = self.cached_entries.get(&entry) {
+                    material_location.diffuse_and_emissive.page = cache.assigned_page_index;
+                }
+            }
+            if let Some(rsod) = descriptor.rsod {
+                let entry = MaterialEntryDescriptor::Rsod(rsod);
+                if let Some(cache) = self.cached_entries.get(&entry) {
+                    material_location.rsod_entry.page = cache.assigned_page_index;
+                }
+            }
+            self.materials.insert(id, material_location);
+        });
+    }
+
+    pub fn build(
+        &mut self,
+        texture_registry: &mut AssetRegistry<RawTexture, TextureMetadata>,
+        material_registry: &mut MaterialLocationRegistry,
+    ) -> MaterialGroup {
+        let size = self.size as i32;
+        let texture = Texture::new_array(
+            size,
+            size,
+            self.pages as i32,
+            MipLevels::default(),
+            MATERIAL_TEXTURE_PIXEL_TYPE,
+            MATERIAL_TEXTURE_FORMAT,
+        );
+
+        // load default blank texture at page 0
+        {
+            let size = self.size as u32;
+            let blank_image = image::RgbaImage::from_pixel(size, size, image::Rgba([255u8; 4]));
+            texture
+                .upload_layer_whole(0, 0, blank_image.as_bytes())
+                .expect("texture is always array texture");
+        }
+
+        let mut image_load_buffer = Vec::new();
+        let pixel_count = (self.size * self.size) as usize;
+        let blank_rgb = vec![255u8; pixel_count * 3];
+        let blank_sc = vec![255u8; pixel_count];
+
+        self.cached_entries
+            .iter_mut()
+            .for_each(|(entry, cache)| match entry {
+                MaterialEntryDescriptor::Rsod(MaterialRsodDescriptor::Coalesced(rgba))
+                | MaterialEntryDescriptor::DiffuseEmissive(
+                    MaterialDiffuseEmissiveDescriptor::Coalesced(rgba),
+                ) => {
+                    let data = rgba.load(texture_registry);
+                    image_load_buffer.extend_from_slice(data.0.as_bytes());
+                }
+                MaterialEntryDescriptor::Rsod(MaterialRsodDescriptor::Separate {
+                    roughness,
+                    specular,
+                    occlusion,
+                    displacement,
+                }) => {
+                    let roughness = roughness.map(|src| src.load(texture_registry));
+                    let specular = specular.map(|src| src.load(texture_registry));
+                    let occlusion = occlusion.map(|src| src.load(texture_registry));
+                    let displacement = displacement.map(|src| src.load(texture_registry));
+                    let r = roughness
+                        .as_ref()
+                        .map_or(blank_sc.as_bytes(), |src| src.0.as_bytes());
+                    let s = specular
+                        .as_ref()
+                        .map_or(blank_sc.as_bytes(), |src| src.0.as_bytes());
+                    let o = occlusion
+                        .as_ref()
+                        .map_or(blank_sc.as_bytes(), |src| src.0.as_bytes());
+                    let d = displacement
+                        .as_ref()
+                        .map_or(blank_sc.as_bytes(), |src| src.0.as_bytes());
+                    coalesce_image_4a(r, s, o, d, &mut image_load_buffer);
+
+                    let image = image::load_from_memory(&image_load_buffer)
+                        .unwrap()
+                        .into_rgba8();
+                    image_load_buffer.clear();
+                    let img_w = image.width();
+                    let img_h = image.height();
+                    cache.norm_sub_width = img_w as f32 / size as f32;
+                    cache.norm_sub_height = img_h as f32 / size as f32;
+                    cache.image = Some(ValidatedRawTexture(image));
+                }
+
+                MaterialEntryDescriptor::DiffuseEmissive(
+                    MaterialDiffuseEmissiveDescriptor::Separate { diffuse, emissive },
+                ) => {
+                    let diffuse = diffuse.map(|src| src.load(texture_registry));
+                    let emissive = emissive.map(|src| src.load(texture_registry));
+                    let d = diffuse
+                        .as_ref()
+                        .map_or(blank_rgb.as_bytes(), |src| src.0.as_bytes());
+                    let e = emissive
+                        .as_ref()
+                        .map_or(blank_sc.as_bytes(), |src| src.0.as_bytes());
+                    coalesce_image_rgb_a(d, e, &mut image_load_buffer);
+
+                    let image = image::load_from_memory(&image_load_buffer)
+                        .unwrap()
+                        .into_rgba8();
+                    image_load_buffer.clear();
+                    let img_w = image.width();
+                    let img_h = image.height();
+                    cache.norm_sub_width = img_w as f32 / size as f32;
+                    cache.norm_sub_height = img_h as f32 / size as f32;
+                    cache.image = Some(ValidatedRawTexture(image));
+                }
+            });
+
+        self.desciptors.drain().for_each(|(id, descriptor)| {
+            let (width, height) = {
+                if let Some(de) = descriptor.diffuse_emissive {
+                    let entry = MaterialEntryDescriptor::DiffuseEmissive(de);
+                    let cache = &self.cached_entries[&entry];
+                    (cache.norm_sub_width, cache.norm_sub_height)
+                } else if let Some(rsod) = descriptor.rsod {
+                    let entry = MaterialEntryDescriptor::Rsod(rsod);
+                    let cache = &self.cached_entries[&entry];
+                    (cache.norm_sub_width, cache.norm_sub_height)
+                } else {
+                    panic!("invalid material {id:?} has zero defined material components");
+                }
+            };
+            let location = self.materials.get_mut(&id).unwrap();
+            location.width = width;
+            location.height = height;
+        });
+
+        MaterialGroup {
+            array_texture_object: texture,
+            size: self.size,
+            pages: self.pages,
+        }
+    }
 }
 
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct MaterialDescriptor {
-    /// diffuse + emissive entry
+    /// Diffuse + emissive entry
     pub diffuse_emissive: Option<MaterialDiffuseEmissiveDescriptor>,
     /// RSOD (roughness-specular-occlusion-displacement) entry
     pub rsod: Option<MaterialRsodDescriptor>,
@@ -363,6 +537,14 @@ pub struct MaterialDescriptor {
 pub enum MaterialEntryDescriptor {
     DiffuseEmissive(MaterialDiffuseEmissiveDescriptor),
     Rsod(MaterialRsodDescriptor),
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct MaterialEntryCache {
+    pub image: Option<ValidatedRawTexture>,
+    pub norm_sub_width: f32,
+    pub norm_sub_height: f32,
+    pub assigned_page_index: u16,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -402,4 +584,102 @@ pub enum MaterialRsodDescriptor {
 pub enum MaterialComponentSource {
     Path(&'static str),
     Asset(AssetId),
+}
+impl MaterialComponentSource {
+    /// Load material component image from path.
+    ///
+    /// # Panics
+    /// If the material component source is not a
+    /// [`Path`](MaterialComponentSource::Path) variant.
+    ///
+    /// The function will also panic if there was an IO read error or image
+    /// load error.
+    pub fn load_path(&self) -> RawTexture {
+        if let Self::Path(path) = self {
+            let mut bytes = Vec::new();
+            match std::fs::read(path) {
+                Ok(loaded) => bytes = loaded,
+                Err(err) => {
+                    tracing::error!(
+                        "failed to read image data from filesystem from path {path}:\n{err}"
+                    )
+                }
+            }
+            RawTexture(image::load_from_memory(&bytes).expect("failed to load image from memory"))
+        } else {
+            panic!("material component source is not defined by local path");
+        }
+    }
+
+    /// Load material component image from asset registry.
+    ///
+    /// # Panics
+    /// If the material component source is not an
+    /// [`Asset`](MaterialComponentSource::Asset) variant.
+    ///
+    /// The function will also panic if the asset does not exist on the
+    /// given asset registry.
+    pub fn load_asset(
+        &self,
+        registry: &mut AssetRegistry<RawTexture, TextureMetadata>,
+    ) -> RawTexture {
+        if let Self::Asset(id) = self {
+            let handle = registry
+                .get_mut(*id)
+                .expect("failed to locate asset in asset registry");
+            if !handle.is_in_memory() {
+                if let Err(err) = handle.load_to_memory(&Default::default()) {
+                    tracing::error!("failed to load asset id {id:?} to memory:\n{err}");
+                }
+            }
+            handle
+                .take_from_memory()
+                .expect("failed to load asset to memory")
+        } else {
+            panic!("material component source is not defined by asset id");
+        }
+    }
+
+    pub fn load(&self, registry: &mut AssetRegistry<RawTexture, TextureMetadata>) -> RawTexture {
+        match self {
+            MaterialComponentSource::Path(_) => self.load_path(),
+            MaterialComponentSource::Asset(_) => self.load_asset(registry),
+        }
+    }
+}
+
+fn coalesce_image_rgb_a(rgb: &[u8], a: &[u8], out: &mut Vec<u8>) {
+    {
+        let c_rgb = rgb.len() / 3;
+        let c_a = a.len();
+        assert!(
+            c_rgb == c_a,
+            "cannot coalesce rgb and single-channel image buffers if the number of pixels is not equal"
+        );
+    }
+
+    a.iter()
+        .zip(rgb.as_chunks::<3>().0)
+        .for_each(|(a, [r, g, b])| {
+            out.push(*r);
+            out.push(*g);
+            out.push(*b);
+            out.push(*a);
+        });
+}
+
+fn coalesce_image_4a(r: &[u8], g: &[u8], b: &[u8], a: &[u8], out: &mut Vec<u8>) {
+    {
+        assert!(
+            r.len() == g.len() && g.len() == b.len() && b.len() == a.len(),
+            "cannot coalesce 4 single-channel image buffers into rgba if the number of pixels is not equal"
+        );
+    }
+    r.iter()
+        .zip(g)
+        .zip(b)
+        .zip(a)
+        .map(|(((&r, &g), &b), &a)| [r, g, b, a])
+        .flatten()
+        .for_each(|b| out.push(b));
 }
