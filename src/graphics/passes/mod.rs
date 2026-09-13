464ab1d34e97e88b868ade2f5737d445e57fea32
errphoenix <herrphoenix20@gmail.com>
2026-09-13 18:52:42 +0200

    fix: orthonormalize TBN matrix in cotangent derivation util function

--- src/graphics/passes/mod.rs
+++ src/graphics/passes/mod.rs
@@ -607,7 +607,12 @@ pub const UTIL_DERIVE_COTANGENT_GRAD: GlslLib = ethel::shader_glsl_lib! {
         vec3 T = dp2perp * duv1.x + dp1perp * duv2.x;
         vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
         float im = inversesqrt(max(dot(T, T), dot(B, B)));
-        //todo: orthonormalize?
-        return mat3(T * im, B * im, n);
+        T *= im;
+        B *= im;
+
+        T -= dot(T, n) * n;
+        B -= dot(B, T) * T - dot(B, n) * n;
+
+        return mat3(T, B, n);
     "
 };
